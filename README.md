73944de2d62dd5f269ca4685e709249ada1e8040
Inês Oliveira <inesfpoliveira@gmail.com>
2020-03-24 16:49:02 +0000

    mostly done

---
+++ README.md
@@ -0,0 +1,6 @@
+## Why making a list?
+Tired of relying in my own memory or 3 different manga-list making websites because none has all manga/light novels I read ever, this is a front-end only solution to a problem I've been having for years. I may re-create this with backend later on.
+
+## Things to be solved:
+- Mobile view (not responsive at all for now, sorry)
+- Pictures (those are placeholders)
---
+++ index.js
@@ -0,0 +1,591 @@
+var manga = [
+    /* Numbers */
+    {
+        title: "07-Ghost",
+        pic: "./pics/07.png",
+        chapter: "99",
+        volume: "17",
+        status: "Complete"
+    },
+    {
+        title: "1001 Knights",
+        pic: "./pics/1001.png",
+        chapter: "55",
+        volume: "10",
+        status: "Complete"
+    },
+
+    /* A */
+    {
+        title: "Aoharu x Kikanjuu",
+        pic: "./pics/aoharu.jpg",
+        chapter: "43",
+        volume: "11",
+        status: "Reading"
+    },
+    {
+        title: "Ao no Exorcist",
+        pic: "./pics/ane.jpg",
+        chapter: "119",
+        volume: "24",
+        status: "Reading"
+    },
+
+    /* B */
+    {
+        title: "Ballad x Opera",
+        pic: "./pics/ballad.png",
+        chapter: "26",
+        volume: "5",
+        status: "Complete"
+    },
+    {
+        title: "Battle Rabbits",
+        pic: "./pics/bato.png",
+        chapter: "9",
+        volume: "2",
+        status: "On-Hold"
+    },
+    {
+        title: "Beyblade Burst",
+        pic: "./pics/bey.jpg",
+        chapter: "3",
+        volume: "1",
+        status: "Reading"
+    },
+    {
+        title: "Bloody Mary",
+        pic: "./pics/bloody.png",
+        chapter: "40",
+        volume: "10",
+        status: "Complete"
+    },
+    {
+        title: "Boku no Tonari ni Ankoku Haikishin ga Imasu.",
+        pic: "./pics/bokutona.png",
+        chapter: "30",
+        volume: "5",
+        status: "Reading"
+    },
+    {
+        title: "Bokura no Sentaku",
+        pic: "./pics/sentaku.jpg",
+        chapter: "5",
+        volume: "2",
+        status: "On-Hold"
+    },
+
+    /* C */
+    {
+        title: "Castehate",
+        pic: "./pics/caste.jpg",
+        chapter: "5",
+        volume: "2",
+        status: "Reading"
+    },
+    {
+        title: "Code:Justice",
+        pic: "./pics/code.jpg",
+        chapter: "6",
+        volume: "1",
+        status: "Complete"
+    },
+
+    /* D */
+    {
+        title: "Daiku No Magia",
+        pic: "./pics/daiku.png",
+        chapter: "10",
+        volume: "2",
+        status: "Complete"
+    },
+    {
+        title: "Danzai no Warlock",
+        pic: "./pics/danzai.jpg",
+        chapter: "3",
+        volume: "1",
+        status: "Complete"
+    },
+    {
+        title: "Darren Shan",
+        pic: "./pics/darren.png",
+        chapter: "2",
+        volume: "1",
+        status: "On-Hold"
+    },
+    {
+        title: "DNAngel",
+        pic: "./pics/dnangel.jpg",
+        chapter: "Stage 4 Vol.35",
+        volume: "18",
+        status: "Reading"
+    },
+
+    /* E */
+    {
+        title: "Eto Royale",
+        pic: "./pics/eto.jpg",
+        chapter: "11",
+        volume: "2",
+        status: "Complete"
+    },
+
+    /* G */
+    {
+        title: "Gaitou Alice",
+        pic: "./pics/gaitou.png",
+        chapter: "16",
+        volume: "3",
+        status: "Complete"
+    },
+    {
+        title: "Game Overdose",
+        pic: "./pics/overdose.jpg",
+        chapter: "11",
+        volume: "2",
+        status: "Complete"
+    },
+
+    /* H */
+    {
+        title: "Hakase Ga!!",
+        pic: "./pics/hakase.png",
+        chapter: "13",
+        volume: "2",
+        status: "Reading"
+    },
+    {
+        title: "Hanamachi Monogatari - Hatsuzakura Mau, Yoru no Shitone",
+        pic: "./pics/hanamachi.png",
+        chapter: "5",
+        volume: "1",
+        status: "Complete"
+    },
+    {
+        title: "Hana, Musubu Kimi He",
+        pic: "./pics/hana.png",
+        chapter: "3",
+        volume: "1",
+        status: "Dropped"
+    },
+
+    /* I */
+    {
+        title: "Ikki no Ki",
+        pic: "./pics/ikki.jpg",
+        chapter: "12",
+        volume: "4",
+        status: "On-Hold"
+    },
+    {
+        title: "Inu x Boku SS",
+        pic: "./pics/inu-x-boku.jpg",
+        chapter: "25",
+        volume: "4",
+        status: "On-Hold"
+    },
+
+    /* J */
+    {
+        title: "Jibaku Shounen Hanako-kun",
+        pic: "./pics/hanako.jpg",
+        chapter: "64",
+        volume: "14",
+        status: "Reading"
+    },
+
+    /* K */
+    {
+        title: "Kami-sama Sacrifice",
+        pic: "./pics/sacrifice.jpg",
+        chapter: "0",
+        volume: "0",
+        status: "Plan to Read"
+    },
+    {
+        title: "Kare Pillow",
+        pic: "./pics/kare.jpg",
+        chapter: "1",
+        volume: "1",
+        status: "Complete"
+    },
+    {
+        title: "Karneval",
+        pic: "./pics/karneval.png",
+        chapter: "105",
+        volume: "18",
+        status: "Reading"
+    },
+    {
+        title: "Kimetsu no Yaiba",
+        pic: "./pics/kimetsu.jpg",
+        chapter: "45",
+        volume: "7",
+        status: "Reading"
+    },
+    {
+        title: "Kyokou Suiri",
+        pic: "./pics/suiri.jpg",
+        chapter: "0",
+        volume: "0",
+        status: "Plan to Read"
+    },
+    {
+        title: "Koiiro Devil",
+        pic: "./pics/koiiro.jpg",
+        chapter: "12",
+        volume: "3",
+        status: "Reading"
+    },
+    {
+        title: "Kuroshitsuji",
+        pic: "./pics/kuroshitsuji.jpg",
+        chapter: "46",
+        volume: "10",
+        status: "Dropped"
+    },
+    {
+        title: "Kyou kara Ma no Tsuku Jiyuugyou!",
+        pic: "./pics/kyou.jpg",
+        chapter: "29",
+        volume: "5",
+        status: "On-Hold"
+    },
+
+    /* L */
+    {
+        title: "Lemure Blue's 2am",
+        pic: "./pics/lemure.png",
+        chapter: "12",
+        volume: "2",
+        status: "Complete"
+    },
+    {
+        title: "Love & Hate",
+        pic: "./pics/love.png",
+        chapter: "11",
+        volume: "2",
+        status: "Complete"
+    },
+
+    /* M */
+    {
+        title: "Makai Ouji - Devils and Realist",
+        pic: "./pics/makai.png",
+        chapter: "94",
+        volume: "15",
+        status: "Complete"
+    },
+    {
+        title: "Mahoutsukai no Yome",
+        pic: "./pics/yome.jpg",
+        chapter: "0",
+        volume: "0",
+        status: "Plan to Read"
+    },
+    {
+        title: "Maou Voice",
+        pic: "./pics/maou.jpg",
+        chapter: "2",
+        volume: "1",
+        status: "On-Hold"
+    },
+    {
+        title: "Mr. Mallow Blue",
+        pic: "./pics/mallow.png",
+        chapter: "2",
+        volume: "1",
+        status: "Reading"
+    },
+    {
+        title: "Momogumi plus Senki",
+        pic: "./pics/momogumi.png",
+        chapter: "107",
+        volume: "20",
+        status: "Reading"
+    },
+
+    /* N */
+    {
+        title: "Nabari no Ou",
+        pic: "./pics/nabari.jpg",
+        chapter: "72",
+        volume: "14",
+        status: "Complete"
+    },
+    {
+        title: "Nightmare Eater",
+        pic: "./pics/nightmare.jpg",
+        chapter: "8",
+        volume: "2",
+        status: "Complete"
+    },
+    {
+        title: "No.6",
+        pic: "./pics/no6.jpg",
+        chapter: "21",
+        volume: "4",
+        status: "On-Hold"
+    },
+
+
+    /* O */
+    {
+        title: "Oiran Jigoku",
+        pic: "./pics/oiran.jpg",
+        chapter: "2",
+        volume: "1",
+        status: "On-Hold"
+    },
+    {
+        title: "Ouji wa Tadaima Dekasegichuu",
+        pic: "./pics/tadaima.png",
+        chapter: "9",
+        volume: "2",
+        status: "Complete"
+    },
+    {
+        title: "Owari no Seraph",
+        pic: "./pics/owasera.png",
+        chapter: "88",
+        volume: "20",
+        status: "Reading"
+    },
+
+    /* P */
+    {
+        title: "Pandora Hearts",
+        pic: "./pics/pandora.png",
+        chapter: "104",
+        volume: "24",
+        status: "Complete"
+    },
+
+    /* R */
+    {
+        title: "Rack - 13-Kei no Zankoku Kikai",
+        pic: "./pics/rack.png",
+        chapter: "2",
+        volume: "1",
+        status: "On-Hold"
+    },
+    {
+        title: "Ragna Crimson",
+        pic: "./pics/ragna.jpg",
+        chapter: "0",
+        volume: "0",
+        status: "Plan to Read"
+    },
+    {
+        title: "Re:Birthday Song - Koi o Utau Shinigami  ",
+        pic: "./pics/rebirthday.jpg",
+        chapter: "3",
+        volume: "1",
+        status: "Complete"
+    },
+    {
+        title: "Risky Crime",
+        pic: "./pics/risky.jpg",
+        chapter: "8",
+        volume: "2",
+        status: "Complete"
+    },
+    {
+        title: "Rokka no Yuusha",
+        pic: "./pics/rokka.jpg",
+        chapter: "2",
+        volume: "5",
+        status: "Reading"
+    },
+
+    /* S */
+    {
+        title: "Seirei Gensouki - Konna Sekai de Deaeta Kimi ni",
+        pic: "./pics/seirei.jpg",
+        chapter: "0",
+        volume: "0",
+        status: "Plan to Read"
+    },
+    {
+        title: "Sekai-ichi Hatsukoi - Onodera Ritsu no Baai",
+        pic: "./pics/sekaiichi.jpg",
+        chapter: "9",
+        volume: "3",
+        status: "On-Hold"
+    },
+    {
+        title: "Sekai-ichi Hatsukoi - Yoshino Chiaki no Baai",
+        pic: "./pics/sekaiichi2.jpg",
+        chapter: "15",
+        volume: "5",
+        status: "On-Hold"
+    },
+    {
+        title: "Sennen Meikyuu no Nana Ouji",
+        pic: "./pics/senmei.png",
+        chapter: "21",
+        volume: "4",
+        status: "Complete"
+    },
+    {
+        title: "Sennen Meikyuu no Nana Ouji -Eikyuu Kairou no Kishi- ",
+        pic: "./pics/senmei2.jpg",
+        chapter: "6",
+        volume: "1",
+        status: "Complete"
+    },
+    {
+        title: "Servamp",
+        pic: "./pics/servamp.jpg",
+        chapter: "70",
+        volume: "14",
+        status: "Reading"
+    },
+    {
+        title: "Setsuna Graffiti",
+        pic: "./pics/setsuna.png",
+        chapter: "6",
+        volume: "2",
+        status: "On-Hold"
+    },
+    {
+        title: "Shinomiya-kun ni wa Wake ga Aru",
+        pic: "./pics/shinomiya.jpg",
+        chapter: "5",
+        volume: "2",
+        status: "Reading"
+    },
+    {
+        title: "Shishunki na Adam - Evil Eyes",
+        pic: "./pics/adam.png",
+        chapter: "30",
+        volume: "4",
+        status: "Complete"
+    },
+    {
+        title: "Shitsuji no Kuni no Kuro Alice",
+        pic: "./pics/shitsujikuni.jpg",
+        chapter: "10",
+        volume: "2",
+        status: "Complete"
+    },
+    {
+        title: "Shunkan Lyle",
+        pic: "./pics/lyle.jpg",
+        chapter: "30",
+        volume: "3",
+        status: "Complete"
+    },
+    {
+        title: "Splatoon",
+        pic: "./pics/splatoon.jpg",
+        chapter: "4",
+        volume: "1",
+        status: "Dropped"
+    },
+
+    /* T */
+    {
+        title: "Tate no Yuusha no Nariagari",
+        pic: "./pics/tate.jpg",
+        chapter: "25",
+        volume: "4",
+        status: "On-Hold"
+    },
+    {
+        title: "Torikago Syndrome",
+        pic: "./pics/torikago.png",
+        chapter: "9",
+        volume: "2",
+        status: "Complete"
+    },
+
+    /* U */
+    {
+        title: "Uragiri wa Boku no Namae wo Shitteiru",
+        pic: "./pics/uraboku.png",
+        chapter: "63",
+        volume: "13",
+        status: "Complete"
+    },
+
+    /* V */
+    {
+        title: "Vampire Knight",
+        pic: "./pics/vampire.jpg",
+        chapter: "93",
+        volume: "19",
+        status: "Complete"
+    },
+    {
+        title: "Vanitas no Carte",
+        pic: "./pics/vanitas.jpg",
+        chapter: "37",
+        volume: "10",
+        status: "Reading"
+    },
+
+    /* W */
+    {
+        title: "Wareware wa Uchuujin Da!!",
+        pic: "./pics/wareware.png",
+        chapter: "10",
+        volume: "2",
+        status: "Complete"
+    },
+    {
+        title: "Watashi wo Motete Dosunda",
+        pic: "./pics/motete.jpg",
+        chapter: "4",
+        volume: "1",
+        status: "Dropped"
+    },
+    {
+        title: "Worldend:Debugger",
+        pic: "./pics/worldend.png",
+        chapter: "16",
+        volume: "3",
+        status: "Complete"
+    },
+
+    /* Y */
+    {
+        title: "Yu-Gi-Oh Arc-V",
+        pic: "./pics/arcv.jpg",
+        chapter: "15",
+        volume: "3",
+        status: "Reading"
+    },
+    {
+        title: "Yuureisen Chronicle",
+        pic: "./pics/yuureisen.jpg",
+        chapter: "6",
+        volume: "2",
+        status: "Reading"
+    },
+    {
+        title: "Yami no Matsuei",
+        pic: "./pics/matsuei.jpg",
+        chapter: "75",
+        volume: "13",
+        status: "Complete? Who knows if this one is finished"
+    },
+];
+
+function init(){
+    for (var i = 0; i < manga.length; i++) {
+        document.getElementById('side_list').innerHTML += "<li class='manga_name' onclick='show(" + i + ")'>" + manga[i].title + "</li>";
+    }
+}
+
+init();
+
+function show(i){
+    document.getElementById('title').innerHTML = manga[i].title;
+    document.getElementById('picture').innerHTML = "<img src='" + manga[i].pic +"' class='picture'>";
+    document.getElementById('chapter').innerHTML = manga[i].chapter;
+    document.getElementById('volume').innerHTML = manga[i].volume;
+    document.getElementById('status').innerHTML = manga[i].status;
+}
+
+show(0);
